592e4ac160643a37d646429a706660c22719e0c4
Gvanca77 <tinatinabramishvili1992@gmail.com>
2025-03-28 07:28:58 -0400

    Add files via upload

---
+++ test.js
@@ -0,0 +1,30 @@
+//let student = {
+//  FirstName : "gvanca", 
+// LastName :"abramishvili",
+// age : 32,
+// Email : "abramishvili.gvanca77@gmail.com",
+// marks : [ 9,10]
+  
+//}
+
+//console.log (student.FirstName)
+
+
+
+let m=45; n=23
+if (m+n) {
+    console.log(m+n)
+}
+else if  (m>n) {  
+    console. log (m)
+}
+else {
+    console. log('n')
+}
+
+
+
+let studentName=('gvanca')
+console. log  ("rise and shine", "gvanca")
+
+console. log
